d91b48a59a144528ff54dbfc6b94ad5a6f30c10b
bogdan-nd <bogdan6401@gmail.com>
2020-12-21 23:14:35 +0200

    add marking mined transactions

--- src/main/kotlin/com/trspo/node/services/MinedBlockProducer.kt
+++ src/main/kotlin/com/trspo/node/services/MinedBlockProducer.kt
@@ -10,13 +10,21 @@ import org.springframework.stereotype.Service
 @Service
 class MinedBlockProducer {
     @Autowired
+    lateinit var transactionService: TransactionService
+    @Autowired
     lateinit var rabbitTemplate: RabbitTemplate
-
     @Value("\${rabbitmq.block-exchange}")
     lateinit var blockExchange: String
+
 
     fun sendMinedBlockToNetwork(minedBlock: Block) {
         rabbitTemplate.convertAndSend(blockExchange, "", minedBlock)
         print("\nSent block to the network\n")
+        markPoolTransactionsMined(minedBlock)
+    }
+
+    fun markPoolTransactionsMined(minedBlock: Block){
+        val transactions = minedBlock.transactionList
+        transactionService.markTransactionMined(transactions)
     }
 }
